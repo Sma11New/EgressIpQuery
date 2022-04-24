94241c80e738874c3915f9c05a39b307aa5c5e68
野原新之助 <53944964+Sma11New@users.noreply.github.com>
2022-04-24 16:46:17 +0800

    测试URL校验正则

---
+++ src/test/java/IsUrl.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class IsUrl {
+    public static boolean isURL(String str) {
+        str = str.toLowerCase();
+        String regex = "^((https|http)?://)"  //https、http、ftp、rtsp、mms
+                + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp的user@
+                + "(([0-9]{1,3}\\.){3}[0-9]{1,3}" // IP形式的URL- 例如：199.194.52.184
+                + "|" // 允许IP和DOMAIN(域名)
+                + "([0-9a-z_!~*'()-]+\\.)*" // 域名- www.
+                + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\\." // 二级域名
+                + "[a-z]{2,6})" // first level domain- .com or .museum
+                + "(:[0-9]{1,5})?" // 端口号最大为65535,5位数
+                + "((/?)|" // a slash isn't required if there is no file name
+                + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
+        return str.matches(regex);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("输入：");
+            String data = scanner.next();
+            System.out.println(isURL(data));
+        }
+    }
+}
